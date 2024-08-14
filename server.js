e6a1952024619a42e77ebd93fc6d9cf6b1ee587c
Noga <nogaanaby@gmail.com>
2024-08-14 23:15:20 +0300

    succed basic form post

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import Participant from './models/Participants.js';
 
 const app = express()
 app.use(cors())
+app.use(express.json());
 
 app.get('/api', async (req, res) => {
     try {
@@ -16,6 +17,24 @@ app.get('/api', async (req, res) => {
         res.status(500).json({ error: 'Failed to fetch participants' });
     }
 });
+
+app.post('/form', async (req, res) => {
+    try {
+        const {  id, country, city, location, dateOfBirth, condition_id } = req.body;
+        var newParticipant={  per_id:id, country, city, location, date_of_birth:dateOfBirth, condition_id }
+        console.log('newParticipant:',newParticipant);
+        Participant.create(newParticipant);
+
+        res.setHeader('Content-Type', 'application/json');
+        res.json({ message: 'Participant added successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to add participant' });
+    }
+});
+
+//works
+// var p = { per_id: 1, country: 'USA', city: 'New York', location: { type: 'Point', coordinates: [40.7128, -74.0060] }, date_of_birth: '1990-01-01', condition_id: 1 };
+// Participant.create(p);
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001')
